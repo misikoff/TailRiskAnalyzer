431124c2737f2688e5b1ddba40d7118ae89b6130
Thomas Misikoff <tmisikoff@gmail.com>
2024-03-04 15:31:05 -0600

    feat: many updates

--- R/sbs_plot.R
+++ R/sbs_plot.R
@@ -17,9 +17,6 @@
 #' ))
 #'
 create_side_by_side_plot <- function(df, use_log = TRUE, quantiles = c()) {
-  # handle use_log
-  # handle different column names
-
   if (use_log) {
     df <- df |>
       dplyr::mutate(value = log(.data$value))
@@ -49,6 +46,24 @@ create_side_by_side_plot <- function(df, use_log = TRUE, quantiles = c()) {
       size = ifelse(.data$is_special, 1, 1)
     )
 
+  hist_df <- df |>
+    dplyr::group_by(.data$index) |>
+    dplyr::summarise(last_value = dplyr::last(.data$value))
+
+  # get geometric mean of hist_df$last_value
+  # careful here to handle 0s not sure
+  # geometric_mean <- exp(mean(log(hist_df$last_value), na.rm = TRUE))
+  # print(geometric_mean)
+
+  median <- median(hist_df$last_value)
+  mean <- mean(hist_df$last_value)
+
+  print(paste("median", median))
+  print(paste("mean", mean))
+
+  min_val <- min(df$value)
+  max_val <- max(df$value)
+
   time_series_plot <- df |>
     ggplot2::ggplot(
       ggplot2::aes(
@@ -60,43 +75,40 @@ create_side_by_side_plot <- function(df, use_log = TRUE, quantiles = c()) {
     ggplot2::geom_line(ggplot2::aes(alpha = .data$opacity)) +
     ggplot2::theme(
       legend.position = "none",
-      panel.background = ggplot2::element_blank(),
+      # panel.background = ggplot2::element_blank(),
       panel.grid.major = ggplot2::element_blank(),
       panel.grid.minor = ggplot2::element_blank(),
-      plot.margin = ggplot2::margin(0, -0.7, 0, 0, "cm")
+      plot.margin = ggplot2::margin(0, -1, 0, 0, "cm")
+    ) +
+    ggplot2::geom_vline(
+      xintercept = max(df$round), color = "black", linewidth = 1
     ) +
     ggplot2::ylab(ifelse(use_log, "Log Value", "Value"))
 
-  hist_df <- df |>
-    dplyr::group_by(.data$index) |>
-    dplyr::summarise(last_value = dplyr::last(.data$value))
 
-  # get geometric mean of hist_df$last_value
-  # careful here to handle 0s not sure
-  # geometric_mean <- exp(mean(log(hist_df$last_value), na.rm = TRUE))
-  # print(geometric_mean)
-  median <- median(hist_df$last_value)
-
-  mean <- mean(hist_df$last_value)
-  print(mean)
 
   hist_plot <- hist_df |>
     ggplot2::ggplot(ggplot2::aes(.data$last_value)) +
-    ggplot2::geom_histogram(binwidth = 0.5, fill = "blue", color = "black") +
+    ggplot2::geom_histogram(binwidth = 0.5, fill = "blue", alpha = 0.4) +
     ggplot2::theme(
       legend.position = "none",
+      # panel.background = ggplot2::element_blank(),
       panel.grid.major = ggplot2::element_blank(),
-      panel.grid.minor = ggplot2::element_blank()
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.ticks.x = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank()
     ) +
     ggplot2::xlab(NULL) +
-    ggplot2::scale_x_continuous(breaks = NULL) +
-    ggplot2::ylab(NULL) +
-    ggplot2::scale_y_continuous(breaks = NULL) +
+    ggplot2::scale_x_continuous(
+      limits = c(min_val, max_val),
+      position = "top"
+    ) +
+    ggplot2::scale_y_continuous(expand = c(0, 0)) +
     ggplot2::geom_vline(
-      xintercept = mean, color = "red", size = 1, linetype = "dashed"
+      xintercept = mean, color = "red", linewidth = 1, linetype = "dashed"
     ) +
     ggplot2::geom_vline(
-      xintercept = median, color = "black", size = 1, linetype = "dashed"
+      xintercept = median, color = "black", linewidth = 1, linetype = "solid"
     ) +
     ggplot2::coord_flip()
 
@@ -104,6 +116,6 @@ create_side_by_side_plot <- function(df, use_log = TRUE, quantiles = c()) {
   egg::ggarrange(time_series_plot, hist_plot,
     ncol = 2, nrow = 1,
     widths = c(2, 1),
-    labels = c("Individual Outcomes", "Density of Outcomes")
+    labels = c("-  Outcome Paths", "-  Density of Outcomes")
   )
 }
